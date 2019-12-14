e89bdb2503618b3a09df58b4a418ed588c0ef170
Kenrick Yuan <kenrickyuan@gmail.com>
2019-12-14 23:03:52 +0100

    How to bind component functions

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
+
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    // Stop form from submitting
+    event.preventDefault();
+
+    // Get text from input
+    console.log(this.myInput);
+    // Change page to /store/textinput
+  }
+
   render() {
     return (
-      <>
-    { /* test comment */}
-        <form className="store-selector">
-          <h2>This is a H2</h2>
-          <input type="text" required placeholder="Store Name"/>
-          <button type="submit">Visit Store -></button>
-        </form>
-      </>
-      )
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Enter a store</h2>
+        <input
+        type="text"
+        ref={this.myInput}
+        required placeholder="Store Name"
+        defaultValue ="Fish N Chips"
+        />
+        <button type="submit">Visit Store -></button>
+      </form>
+      );
   }
 }
 
