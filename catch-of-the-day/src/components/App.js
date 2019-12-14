635646336d134b40c95ff78409c19a53e6ab1279
Kenrick Yuan <kenrickyuan@gmail.com>
2019-12-15 00:01:03 +0100

    understanding React states

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,22 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = (fish) => {
+    // Take a copy of the existing state (to avoid mutation)
+    const fishesCopy = { ...this.state.fishes };
+    // Add new fish to this fishes copy
+    fishesCopy[`fish${Date.now()}`] = fish;
+    // Set new fishes object to state
+    this.setState({
+      fishes: fishesCopy
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +27,7 @@ class App extends React.Component {
           <Header tagline="Kenrick is cool" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish = {this.addFish} />
       </div>
       );
   }
