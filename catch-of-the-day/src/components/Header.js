a7838927a39280592b3095fc2304f73480312b50
Kenrick Yuan <kenrickyuan@gmail.com>
2019-12-14 21:21:59 +0100

    Stateless Functional Components

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,21 +1,59 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-        <header className="top">
-          <h1>Catch
-            <span className="ofThe">
-              <span className="of">Of</span>
-              <span className="the">The</span>
-            </span>
-          </h1>
-          <h3 className="tagline">
-            <span>{this.props.tagline}</span>
-          </h3>
-        </header>
-      )
-  }
-}
+
+// Can also strip parenthesis around (props) or use ({ tagline })
+
+const Header = (props) => (
+  <header className="top">
+    <h1>Catch
+      <span className="ofThe">
+        <span className="of">Of</span>
+        <span className="the">The</span>
+      </span>
+    </h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
+
+// Old Stateless Functional Component before refactoring
+
+// function Header(props) {
+//   return (
+//     <header className="top">
+//       <h1>Catch
+//         <span className="ofThe">
+//           <span className="of">Of</span>
+//           <span className="the">The</span>
+//         </span>
+//       </h1>
+//       <h3 className="tagline">
+//         <span>{props.tagline}</span>
+//       </h3>
+//     </header>
+//     );
+// }
+
+
+// No need to create an actual React Component for something that just takes data and renders JSX
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//         <header className="top">
+//           <h1>Catch
+//             <span className="ofThe">
+//               <span className="of">Of</span>
+//               <span className="the">The</span>
+//             </span>
+//           </h1>
+//           <h3 className="tagline">
+//             <span>{this.props.tagline}</span>
+//           </h3>
+//         </header>
+//       );
+//   }
+// }
 
 export default Header;
