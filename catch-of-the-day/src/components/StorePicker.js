5082d903ebc153495a99c5e880993cde1975dccb
Kenrick Yuan <kenrickyuan@gmail.com>
2019-12-14 11:58:25 +0100

    can I push this?:

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <>
+    { /* test comment */}
+        <p>hello</p>
+        <form className="store-selector">
+          <h2>This is a H2</h2>
+          <input type="text" required placeholder="Store Name"/>
+          <button type="submit">Visit Store -></button>
+        </form>
+      </>
+      )
+  }
+}
+
+export default StorePicker;
